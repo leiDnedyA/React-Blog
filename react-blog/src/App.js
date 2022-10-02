d31b92e3c44631df2633414cb2dc91102f93315d
Ayden <aydendiel@gmail.com>
2022-10-02 18:24:20 -0400

    Added navbar w/ links

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -7,10 +7,15 @@ import Login from './pages/Login';
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/createpost"> CreatePost </Link>
+        <Link to="/login"> Login </Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/createpost" element={<CreatePost />}/>
-        <Route path="/login" elment={<Login />}/>
+        <Route path="/login" element={<Login />}/>
       </Routes>
     </Router>
   );
