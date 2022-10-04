52c376c792313653731ff71b93b3cbf60bda85b5
Ayden <aydendiel@gmail.com>
2022-10-04 16:25:03 -0400

    Added title to navbar

--- react-blog/src/elements/NavBar.js
+++ react-blog/src/elements/NavBar.js
@@ -10,6 +10,7 @@ import { Link } from 'react-router-dom'
 function NavBar(props) {
     return (
         <nav className="navBar">
+            <p>{props.title}</p>
             {
             //convert links 
             props.links.map(v => (
