7dcbc4d9258d0329bac825372b0a38a9ffe991d6
Ayden <aydendiel@gmail.com>
2022-10-12 10:47:40 -0400

    set up react app

--- react-blog/src/pages/CreatePost.js
+++ react-blog/src/pages/CreatePost.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function CreatePost() {
-    return <div>CreatePost</div>
+    return (<div>
+        <p>placeholder</p>
+        <button onClick={() => {
+            fetch('/api').then(result => {
+                document.querySelector('#testText').innerHTML = JSON.stringify(result.json());
+                console.log(result)
+            })
+        }}>Test button</button>
+        <p id="testText">test</p>
+    </div>)
 }
 
 export default CreatePost;
