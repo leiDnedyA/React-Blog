a3b7e5a63faf535e16bab1f0199250b9ee1402a0
Ayden <aydendiel@gmail.com>
2022-10-12 11:29:23 -0400

    Added firebase boilerplate

--- server/app.js
+++ server/app.js
@@ -1,4 +1,19 @@
+const { initializeApp } = require('firebase/app');
+const { getFirestore, collection, getDocs } = require('firebase/firestore/lite');
 const express = require("express");
+require('dotenv').config();
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "ayden-s-tech-blog.firebaseapp.com",
+    projectId: "ayden-s-tech-blog",
+    storageBucket: "ayden-s-tech-blog.appspot.com",
+    messagingSenderId: "982619150754",
+    appId: "1:982619150754:web:d8f4f78b5ebb4862cb7afb"
+};
+
+const firebaseApp = initializeApp(firebaseConfig)
+const db = getFirestore(firebaseApp);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,6 +23,5 @@ app.listen(PORT, () => {
 });
 
 app.get("/api", (req, res) => {
-    console.log('server pinged?!')
     res.json({ message: "Hello from server!" });
 });
