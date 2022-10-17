81da2f898555a7beb69c8079707a5266f2c1daca
Ayden <aydendiel@gmail.com>
2022-10-17 11:22:52 -0400

    Moved the BrowserRouter component to index.js

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Login from './pages/Login';
@@ -37,7 +37,6 @@ function App() {
   } else {
     return (
       <div className="app">
-        <Router>
           <AuthProvider>
             <Navbar
               links={
@@ -56,7 +55,6 @@ function App() {
               </Routes>
             </div>
           </AuthProvider>
-        </Router>
       </div>
     );
   }
