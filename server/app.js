5ee431aa3388a53a57c440e9562dea0baa7f3568
Ayden <aydendiel@gmail.com>
2022-10-25 10:21:59 -0400

    switched back to HTTP for time being

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require('fs');
-const https = require('https');
-const path = require('path');
+// const https = require('https');
+// const path = require('path');
 const bodyParser = require("body-parser");
 require('dotenv').config();
 
@@ -23,18 +23,18 @@ const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestor
 
 const serviceAccount = require('../ayden-s-tech-blog-firebase-adminsdk-7sbwz-b082735b48.json');
 
-initializeApp({
-    credential: cert(serviceAccount)
-});
+// initializeApp({
+//     credential: cert(serviceAccount)
+// });
 
 const db = getFirestore();
 
 //setting up express and https stuff VVV
 
-const httpsOptions = {
-    cert: fs.readFileSync(path.join(__dirname, 'ssl', 'server.crt')),
-    key: fs.readFileSync(path.join(__dirname, 'ssl', 'server.key'))
-}
+// const httpsOptions = {
+//     cert: fs.readFileSync(path.join(__dirname, 'ssl', 'server.crt')),
+//     key: fs.readFileSync(path.join(__dirname, 'ssl', 'server.key'))
+// }
 
 const PORT = process.env.PORT || 3001;
 const app = express();
