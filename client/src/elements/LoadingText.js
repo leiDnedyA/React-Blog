162b322ab489a687a2a8aced779f3eb75410b214
Ayden <aydendiel@gmail.com>
2022-10-30 15:53:01 -0400

    animated LoadingText.js

--- client/src/elements/LoadingText.js
+++ client/src/elements/LoadingText.js
@@ -1,2 +1,34 @@
+import { useEffect, useState } from "react"
 
-export default _=><p>Loading...</p>
+/**
+ * Fun animated loading text React element
+ */
+export default ()=>{
+    
+    const [dots, setDots] = useState(1);
+    const dotDelay = 300;
+    const [minDots, maxDots] = [1, 15]
+
+    let interval = null;
+
+    useEffect(()=>{
+        interval = setInterval(()=>{
+            switch (dots) {
+                case maxDots:
+                    setDots(minDots);
+                    break;
+                default:
+                    setDots(dots + 1);
+                    break;
+            };
+        }, dotDelay);
+
+        return function cleanup(){
+            clearInterval(interval);
+        }
+
+    })
+
+    
+    return <p className="loadingText">loading{'.'.repeat(dots)}</p>
+}
