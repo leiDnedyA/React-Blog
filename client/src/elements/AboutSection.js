d8718ffd5712e7e93a41bc12cc1185fb62b04440
Ayden <aydendiel@gmail.com>
2022-11-07 12:33:44 -0500

    changed social links and edited about section

--- client/src/elements/AboutSection.js
+++ client/src/elements/AboutSection.js
@@ -1,13 +1,16 @@
-import { FaGithub, FaInstagramSquare } from 'react-icons/fa';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default () =>
     <div className="about about-grid-container">
         <h3 className="about-header">
             About Me
         </h3>
-        <div className="about-social-links"><FaGithub /><FaInstagramSquare /></div>
+        <div className="about-social-links">
+            <a href="https://github.com/leiDnedyA" className="socials-link"><FaGithub /></a>
+            <a href="https://www.linkedin.com/in/ayden-diel-9b0972191/" className="socials-link"><FaLinkedin /></a>
+            </div>
         <div className="about-body">
             <p>I'm Ayden Diel, a CS undergrad student currently syudying at UMass Boston.</p>
-            <p>I used the FERN stack (Firebase, Express, React, Node) to build this site, and used Heroku to deploy it.</p>
+            <p>You might be wondering what's up with all of the random article names. They're like that because this project is a WIP, but I still want this homepage to give a sense of how the project will look when it's finished.</p>
         </div>
     </div>
